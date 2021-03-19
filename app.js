a706815c1fef94d80013b326a18254b5a1024214
Rishika-05 <rishikaraj7263@gmail.com>
2021-03-19 13:28:17 +0530

    prompt

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 $(document).ready(function()
 {
-for(var count=1;count<=100;count++)
-{if(count%3==0 && count%5==0)
+	findFizzBuzz();
+});
+
+function findFizzBuzz(){
+var num=parseInt(prompt("The number you would like to find FizzBuzz?"));
+for(var count=1;count<=num;count++)
+{
+if(count%3==0 && count%5==0)
 $('.out').append("FizzBuzz</br>");
 else if(count%5==0)
 $('.out').append("Fizz</br>");
@@ -10,4 +16,4 @@ $('.out').append("Buzz</br>");
 else 
 $('.out').append(count+'</br>');
 }
-});
+}
